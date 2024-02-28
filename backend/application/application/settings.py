25128232809b90b043a0cf154edb74bf47491059
Ameer Shaik <shaikameerbasha24@gmail.com>
2024-02-28 22:24:55 +0530

    register commons and tasks apps in INSTALLED_APPS

--- backend/application/application/settings.py
+++ backend/application/application/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "commons.apps.CommonsConfig",
+    "tasks.apps.TasksConfig",
 ]
 
 MIDDLEWARE = [
