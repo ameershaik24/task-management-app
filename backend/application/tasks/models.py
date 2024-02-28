ae716e2774163e51d88c513bc93bbe06c4cf4b4d
Ameer Shaik <shaikameerbasha24@gmail.com>
2024-02-28 22:49:39 +0530

    create Task model, serializer, ListCreate view and api endpoint

--- backend/application/tasks/models.py
+++ backend/application/tasks/models.py
@@ -1,3 +1,15 @@
+from commons.models import BaseModel
 from django.db import models
 
-# Create your models here.
+
+class Task(BaseModel):
+    TODO = "todo"
+    IN_PROGRESS = "in_progress"
+    DONE = "done"
+    TASK_STATUSES = [(TODO, "To DO"), (IN_PROGRESS,
+                                       "In Progress"), (DONE, "Done")]
+
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+
+    status = models.CharField(max_length=20, choices=TASK_STATUSES)
