f7ba72839ae7aff176ad7860e2d46b2fa9abd1b4
Ameer Shaik <shaikameerbasha24@gmail.com>
2024-03-02 12:58:58 +0530

    create and set LandingPageComponent

--- frontend/task-management/src/app/app.module.ts
+++ frontend/task-management/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./components/header/header.component";
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LandingPageComponent,
   ],
   imports: [
     BrowserModule,
